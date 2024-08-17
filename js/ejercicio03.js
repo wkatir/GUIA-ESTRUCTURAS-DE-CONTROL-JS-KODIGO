aa8856fd1e047ba92f6bf7713842382f7d63a061
wkatir <110741874+wkatir@users.noreply.github.com>
2024-08-17 13:21:12 -0600

    add fuctionality to the exercise 3 and 4 files

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -10,3 +10,94 @@ Calcular el aumento de trabajador tomando en cuenta la tabla de categorías de a
 Para este ejercicio deberá de asignar las siguientes variables: nombre, salario, categoría y
 aumento. Deberá demostrar los datos del empleado y el aumento salarial. */
 
+const salaryIncrease = () => {
+  const increaseA = 0.15;
+  const increaseB = 0.3;
+  const increaseC = 0.1;
+  const increaseD = 0.2;
+  const baseSalary = parseFloat(prompt("Ingrese el salario del empleado: "));
+  const employeeName = prompt("Ingrese el nombre del empleado: ");
+  let category = prompt(
+    "Ingrese la categoría del empleado (A, B, C, D)"
+  ).toUpperCase();
+
+  if (isNaN(baseSalary) || baseSalary <= 0) {
+    alert("El salario ingresado no es válido.");
+    return;
+  }
+
+  let finalSalary = baseSalary;
+
+  switch (category) {
+    case "A":
+      finalSalary += baseSalary * increaseA;
+      alert(
+        `El empleado ${employeeName} con categoría ${category} tiene un salario final de $${finalSalary.toFixed(
+          2
+        )}`
+      );
+      break;
+    case "B":
+      finalSalary += baseSalary * increaseB;
+      alert(
+        `El empleado ${employeeName} con categoría ${category} tiene un salario final de $${finalSalary.toFixed(
+          2
+        )}`
+      );
+      break;
+    case "C":
+      finalSalary += baseSalary * increaseC;
+      alert(
+        `El empleado ${employeeName} con categoría ${category} tiene un salario final de $${finalSalary.toFixed(
+          2
+        )}`
+      );
+      break;
+    case "D":
+      finalSalary += baseSalary * increaseD;
+      alert(
+        `El empleado ${employeeName} con categoría ${category} tiene un salario final de $${finalSalary.toFixed(
+          2
+        )}`
+      );
+      break;
+    default:
+      alert(
+        `Categoría ${category} no válida. Por favor, ingrese una categoría válida (A, B, C, D).`
+      );
+      return;
+  }
+};
+
+salaryIncrease();
+
+/* const employeeName = "Arturo";
+const salary = 1000;
+let category = "D";
+const increaseA = 0.15;
+const increaseB = 0.30;
+const increaseC = 0.10;
+const increaseD = 0.20;
+let increasefinal = salary; 
+
+switch(category) {
+    case "A":
+        increasefinal += salary * increaseA;    
+        console.log(`El salario final es ${increasefinal} por categoria A`);
+        break;
+    case "B":
+        increasefinal += salary * increaseB;
+        console.log(`El salario final es ${increasefinal} por categoria B`);
+        break;
+    case "C":
+        increasefinal += salary * increaseC;
+        console.log(`El salario final es ${increasefinal} por categoria C`);
+        break;
+    case "D":
+        increasefinal += salary * increaseD;
+        console.log(`El salario final es ${increasefinal} por categoria D`);
+        break;
+    default:
+        console.log("Ingrese una opcion valida");    
+}
+ */
--- js/ejercicio04.js
+++ js/ejercicio04.js
@@ -3,7 +3,30 @@
 /* EJERCICIO 4: Crear una función que en base a 2 números enteros que ingrese el usuario,
 calcular cual número es el mayor y devolverlo.  */
 
-let number1 = 2;
+const findMaximum = (number1, number2) => {
+    if(isNaN(number1) || isNaN(number2)){
+        alert("Ambas entradas deben ser números válidos");
+        return;
+    }
+
+    if(number1 > number2){
+        return number1;
+    }else if(number2 > number1){
+        return number2;
+    }else{
+        return 'Ambos números son iguales';
+    }
+}
+
+let number1 = parseInt(prompt("Ingresa el primer número entero: "));
+let number2 = parseInt(prompt("Ingresa el segundo número entero: "));
+
+const maximumNumber = findMaximum(number1, number2);
+alert(`El número máximo es: ${maximumNumber}`);
+
+
+
+/* let number1 = 2;
 let number2 = 3;
 
 const verifyGreater = (number1, number2) => {
@@ -16,4 +39,4 @@ const verifyGreater = (number1, number2) => {
     }
 }
 
-verifyGreater(number1, number2);
+verifyGreater(number1, number2); */
