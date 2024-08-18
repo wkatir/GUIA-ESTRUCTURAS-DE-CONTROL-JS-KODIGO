61eb059bd6d11aa822a57a8c40c66962195c5a80
wkatir <110741874+wkatir@users.noreply.github.com>
2024-08-17 18:45:35 -0600

    adding structure and fuctionality to the 6th exercise

--- js/ejercicio06.js
+++ js/ejercicio06.js
@@ -6,3 +6,43 @@ Si el usuario introduce como origen la ciudad de Palma y como destino La costa d
 descuento será de 5%, si el destino es Panchimalco el descuento será del 10% y si el destino
 es Puerto el Triunfo el descuento será del 15%. */
 
+const calculateDiscount = () => {
+  const origin = prompt("Introduce la ciudad de origen: ").toLowerCase();
+  const destination = prompt("Introduce el destino: ").toLowerCase();
+  const baseCost = parseFloat(prompt("Introduce el costo del viaje: "));
+  if (baseCost === "" || isNaN(baseCost) || parseFloat(baseCost) <= 0) {
+    alert("Por favor, introduce un costo válido (un número mayor que 0).");
+    return;
+  }
+  let discount = 0;
+  if (origin === "palma") {
+    switch (destination) {
+      case "la costa del sol":
+        discount =  0.05;
+        break;
+      case "panchimalco":
+        discount = 0.10;
+        break;
+      case "puerto el triunfo":
+        discount = 0.15;
+        break;
+      default:
+        discount = 0;
+        break;
+    }
+  }
+
+  if (discount > 0) {
+    const discountedCost = baseCost - baseCost * discount;
+    alert(
+      `El descuento aplicable es del ${(discount * 100).toFixed(
+        2
+      )}%. El costo del viaje con descuento es: $${discountedCost.toFixed(2)}`
+    );
+  } else {
+    alert(
+      "No hay descuento aplicable para el destino ingresado. El costo del viaje es: $" +
+        baseCost.toFixed(2)
+    );
+  }
+};
